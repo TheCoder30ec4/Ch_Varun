294d905418b4544571283e195cc4f0e50e6477ba
TheCoder30ec4 <varun30ec4@gmail.com>
2025-06-15 01:47:33 +0530

    vite

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base:"/Protfolio",
   plugins: [react()],
-  root: resolve(__dirname, 'public'),
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
+  root: '.',
+  build:{
+    outDir:'dist',
+    emptyOutDir:true,
   },
 })
-
